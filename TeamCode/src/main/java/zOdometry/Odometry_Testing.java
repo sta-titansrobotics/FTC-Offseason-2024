3a63acda1c97aa5310dbc5d15a3394802aa508bb
Gordon <gordon.zhu25@ycdsbk>
2024-04-23 15:02:13 -0400

    revert to the thing before this commit, if something goes wrong

--- TeamCode/src/main/java/zOdometry/Odometry_Testing.java
+++ TeamCode/src/main/java/zOdometry/Odometry_Testing.java
@@ -70,7 +70,6 @@ public class Odometry_Testing extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            double max; //
 
             //calculation of raw encoder movement
             //lf --> x1 on the left
